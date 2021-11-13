c42a450920f6100233461d7dcff629fab6bbed92
Julio Zittei <julio.zittei96@gmail.com>
2021-11-13 01:36:56 -0300

    :tada: Challenge completed

---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">WatchMe</h1>
+
+<p align="center">
+  <a href="#-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-how-to-run">How to run</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-license">License</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=8257E5&labelColor=000000">
+</p>
+
+<p align="center">
+  <img alt="app" src=".github/WatchMe.gif" width="100%">
+</p>
+
+## ✨ Technologies
+
+This project was developed with the following technologies:
+
+- [React](https://pt-br.reactjs.org/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+## 🚀 How to run
+
+- Clone the repository and access the folder;
+- Install the dependencies with `yarn`;
+- Start the json server with `yarn server`;
+- Start the server with `yarn dev`;
+
+The application can be accessed at [`localhost:8080`](http://localhost:8080)
+
+You can access the live web application [`clicking here`]().
+
+## 📄 License
+
+This project is under the MIT license. See the [LICENSE](LICENSE) file for more details.
+
+---
+
+Made with ♥ by Julio Zittei 👋🏻
---
+++ src/contexts/MoviesContext.tsx
@@ -0,0 +1,75 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from '../services/api';
+
+export interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface MoviesContextProviderData {
+  genres: GenreResponseProps[];
+  selectedGenreId: number;
+  movies: MovieProps[];
+  selectedGenre: GenreResponseProps;
+  handleClickButton: (id: number) => void;
+}
+
+interface MoviesContextProviderProps {
+  children: ReactNode;
+}
+
+export const MoviesContext = createContext({} as MoviesContextProviderData);
+
+export function MoviesContextProvider({ children }: MoviesContextProviderProps) {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then((response) => {
+      setMovies(response.data);
+    });
+
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then((response) => {
+      setSelectedGenre(response.data);
+    });
+  }, [selectedGenreId]);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+  return (
+    <MoviesContext.Provider
+      value={{
+        genres,
+        movies,
+        selectedGenreId,
+        selectedGenre,
+        handleClickButton,
+      }}
+    >
+      {children}
+    </MoviesContext.Provider>
+  );
+}
